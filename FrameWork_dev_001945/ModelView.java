9d28533a7a81d61d38b7f92dfbd9382fea8ce9dd
f7a1m2 <faments256@gmail.com>
2023-07-01 14:48:08 +0300

    Sprint6

--- FrameWork_dev_001945/ModelView.java
+++ FrameWork_dev_001945/ModelView.java
@@ -1,16 +1,29 @@
 package etu1945.framework;
 
+import java.util.HashMap;
+
 public class ModelView {
     // chemin pour atteindre les pages .jsp
     String url;
+    HashMap<String, Object> data = new HashMap<>();
 
     public void setUrl(String url) {
-    this.url = url;
+        this.url = url;
     }
 
     public String getUrl() {
         return url;
     }
 
+    public void setData(HashMap data) {
+        this.data = data;
+    }
 
+    public HashMap<String, Object> getData() {
+        return data;
+    }
+
+    public void addItem(String key, Object value) {
+        this.getData().put(key, value);
+    }
 }
