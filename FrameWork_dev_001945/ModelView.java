64c69eb3f4e73b9dfc64bb5e1e65038d281a5139
f7a1m2 <faments256@gmail.com>
2023-07-01 13:41:54 +0300

    changement

--- FrameWork_dev_001945/ModelView.java
+++ FrameWork_dev_001945/ModelView.java
@@ -3,14 +3,23 @@ package etu1945.framework;
 public class ModelView {
     // chemin pour atteindre les pages .jsp
     String url;
+    HashMap data;
 
     public void setUrl(String url) {
-    this.url = url;
+        this.url = url;
     }
 
     public String getUrl() {
         return url;
     }
 
+    public void setData(HashMap data) {
+        this.data = data;
+    }
 
+    public HashMap getData() {
+        return data;
+    }
+
+    public 
 }
