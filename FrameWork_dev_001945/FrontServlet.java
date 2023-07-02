63964dbaea31f9615f2b069ab3e61dfb20724945
f7a1m2 <faments256@gmail.com>
2023-07-02 12:58:02 +0300

    sprint7

--- FrameWork_dev_001945/FrontServlet.java
+++ FrameWork_dev_001945/FrontServlet.java
@@ -5,20 +5,21 @@ import java.util.*;
 import java.beans.MethodDescriptor;
 import java.io.File;
 import java.net.URL;
+import java.net.URLConnection;
 import java.net.URI;
 import java.lang.Package;
 import jakarta.servlet.*;
+import java.lang.reflect.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.http.*;
-import java.lang.reflect.Method;
 import java.net.URISyntaxException;
 
 import etu1945.framework.*;
 
 public class FrontServlet extends HttpServlet {
 
-    HashMap<String,Mapping> MappingUrls = new HashMap<>();
+    HashMap<String, Mapping> MappingUrls = new HashMap<>();
 
     public  void doGet(HttpServletRequest request, HttpServletResponse response) 
         throws ServletException, IOException  {
@@ -30,12 +31,25 @@ public class FrontServlet extends HttpServlet {
 
 	}
 
+    public String capitaliz(String input) {
+        if (input == null || input.isEmpty()) {
+            return input;
+        }
+
+        String firstChar = input.substring(0 , 1).toUpperCase();
+        String remainingChars = input.substring(1);
+
+        return firstChar + remainingChars;
+    }
+
     public void processRequest(HttpServletRequest request, HttpServletResponse response) 
     throws ServletException, IOException  {
     
         response.setContentType("text/html");
         PrintWriter out = response.getWriter(); 
         String url = request.getRequestURI().substring(request.getContextPath().length());
+        // Map<Stirng, String[]> paramterMap = request.getParameterMap();
+        Enumeration<String> parameterNames = request.getParameterNames();
         
         out.println("<h1>" + this.MappingUrls + "</h1>");
         out.println("<h1>" + request.getRequestURI().substring(request.getContextPath().length()) + "</h1>");
@@ -45,17 +59,34 @@ public class FrontServlet extends HttpServlet {
                 Method meth = null;
                 Mapping map = MappingUrls.get(url);
                 Class<?> classmap = Class.forName(map.getClassName());
+                Field[] fields = classmap.getDeclaredFields();
                 Method[] methods = classmap.getDeclaredMethods();
+                Object myClass = classmap.getDeclaredConstructor().newInstance();
+
                 for (int i = 0; i < methods.length; i++) {
                     if (methods[i].getName().equals(map.getMethod())) {
                         meth = methods[i];
                     }
                 }
-    
-                Object myClass = classmap.getDeclaredConstructor().newInstance();
+
+                for (int i = 0; i < fields.length ; i++) {
+                    String fieldName = fields[i].getName();
+                    
+                    while (parameterNames.hasMoreElements()) {
+                        String paramName = parameterNames.nextElement();
+                        if (paramName.equals(fieldName)) {
+                            String cloud = request.getParameter(paramName);
+                            Method getSet = classmap.getDeclaredMethod("set"+capitaliz(fieldName), fields[i].getType());
+
+                            Object saver= fields[i].getType().getConstructor(String.class).newInstance(cloud);
+                            getSet.invoke(myClass, saver);
+                        }
+                    }
+                }
+
                 Object[] parameters = null;
                 Object retour = meth.invoke(myClass, parameters);
-    
+                
                 if (retour instanceof ModelView) {
                     ModelView cView = (ModelView) retour;
                     HashMap<String, Object> dataValue = new HashMap<>();
@@ -65,7 +96,9 @@ public class FrontServlet extends HttpServlet {
                     }
                     request.getRequestDispatcher(cView.getUrl()).forward(request, response);
                 }
+
             }
+
         } catch (Exception e) {
             e.printStackTrace(out);
         }
